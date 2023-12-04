1a9064f6376f307063f87d59a4393100e337d345
RdelatowerPR <109052713+RdelatowerPR@users.noreply.github.com>
2023-12-04 04:21:17 -0500

    Modified files
    Homescreen looks fine
    Personscreen now can take values although get error for "non-serializable values were found in the navigator"
    Also clicking on NextPerson screws everything up

--- PersonScreen.js
+++ PersonScreen.js
@@ -1,31 +1,41 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
 
 const PersonScreen = ({ route, navigation }) => {
-    const { personIndex, person, handlePersonChange, handleNextPerson } = route.params;
+    const { personIndex, person } = route.params;
 
+    // Initialize local state for person's data
+    const [localPerson, setLocalPerson] = useState(person);
     const updateField = (field, value) => {
-        handlePersonChange(personIndex, field, value);
+        // Handling for order amount and tip percentage
+        if (field === 'orderAmount' || field === 'tipPercentage') {
+            const numericValue = value === '' ? '' : parseFloat(value);
+            if (!isNaN(numericValue) || value === '') {
+                setLocalPerson({ ...localPerson, [field]: numericValue });
+            }
+        } else {
+            setLocalPerson({ ...localPerson, [field]: value });
+        }
     };
-
     const goToNextPerson = () => {
-        handleNextPerson(personIndex, navigation);
+        const nextPersonIndex = personIndex + 1;
+        // Navigate to the next screen or person screen
+        navigation.navigate('PersonScreen', { personIndex: nextPersonIndex });
     };
-
     return (
         <View style={styles.container}>
             <Text style={styles.title}>{`Person ${personIndex + 1}`}</Text>
             <TextInput 
                 style={styles.input}
                 placeholder="Enter order amount"
-                value={person.orderAmount.toString()}
+                value={localPerson.orderAmount?.toString()}
                 onChangeText={text => updateField('orderAmount', text)}
                 keyboardType="numeric"
             />
             <TextInput 
                 style={styles.input}
                 placeholder="Enter tip percentage"
-                value={person.tipPercentage.toString()}
+                value={localPerson.tipPercentage?.toString()}
                 onChangeText={text => updateField('tipPercentage', text)}
                 keyboardType="numeric"
             />
@@ -33,7 +43,6 @@ const PersonScreen = ({ route, navigation }) => {
         </View>
     );
 };
-
 const styles = StyleSheet.create({
     container: {
         flex: 1,
